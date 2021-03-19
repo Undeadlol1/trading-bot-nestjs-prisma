67d119b35b554f48592efce27aaedd5442e16226
undeadlol1 <undeadlol1@mail.ru>
2021-03-19 18:48:02 +0500

    refactor: :recycle: minor

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -51,7 +51,5 @@ async function main() {
 }
 
 main()
-  .catch((e) => console.error(e))
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+  .catch(console.error)
+  .finally(async () => await prisma.$disconnect());
